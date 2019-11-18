bd45acac8432488e5f768c63f266bfc2b582ec88
Del Santos <delprojetosweb@gmail.com>
2019-11-18 18:53:28 -0300

    Correção Erros

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -9,9 +9,10 @@ class Page {
     private $tpl;
     private $options = [];
     private $defaults = [
+        "header"=>true,
+        "footer"=>true,
         "data"=>[]
     ];
-
 
     public function __construct($opts = array(), $tpl_dir = "/views/"){
 
@@ -29,7 +30,7 @@ class Page {
 
         $this->setData($this->options["data"]);
 
-        $this->tpl->draw("header");
+       if ($this->options["header"] === true) $this->tpl->draw("header");
 
     }
 
@@ -52,10 +53,8 @@ class Page {
 
     public function __destruct() {
   
-         $this->tpl->draw("footer");
-        
-       
-        
+        if ($this->options["footer"] === true) $this->tpl->draw("footer");
+               
     }
 
 }
